0849e609b9ebdc05b785a3c93a9b528a2794a5d8
chiayewken <chiayewken@gmail.com>
2024-02-27 10:51:32 +0800

    Add text splitting in ClipTextRetriever due to max tokens limit (77)

--- retrieval.py
+++ retrieval.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
+import numpy as np
 from fire import Fire
+from nltk import sent_tokenize
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
 from sentence_transformers.util import cos_sim
@@ -19,24 +21,57 @@ class MultimodalRetriever(BaseModel, arbitrary_types_allowed=True):
 class ClipTextRetriever(MultimodalRetriever):
     model_path: str = "clip-ViT-L-14"
     model: Optional[SentenceTransformer] = None
+    max_length: int = 77
 
     def load(self):
         if self.model is None:
             self.model = SentenceTransformer("clip-ViT-L-14")
+
+    def check_length(self, text) -> int:
+        return self.model.tokenize([text]).input_ids.shape[1]
+
+    def truncate_text(self, text: str, sep: str = " ") -> str:
+        if self.check_length(text) <= self.max_length:
+            return text
+
+        words = text.split(sep)
+        while self.check_length(sep.join(words)) > self.max_length:
+            words = words[:-1]
+
+        new = sep.join(words)
+        print(dict(original=self.check_length(text), truncated=self.check_length(new)))
+        return new
 
     def run(
         self, query: MultimodalObject, doc: MultimodalDocument
     ) -> MultimodalDocument:
         self.load()
         doc = doc.copy(deep=True)
-        texts = [x.text for x in doc.objects]
-        query = self.model.encode(query.text)
-        data = self.model.encode(texts)
-        scores = cos_sim(query, data).squeeze().tolist()
-        assert len(scores) == len(doc.objects)
+        query_embeds = self.model.encode(query.text)
 
-        for i, o in enumerate(doc.objects):
-            o.score = scores[i]
+        # Split long texts to avoid CLIP max context length error (77 tokens)
+        groups = {}
+        for i, x in enumerate(doc.objects):
+            if x.image_string:
+                x.text = self.truncate_text(x.text)
+                groups[i] = [x]
+            else:
+                assert x.text
+                for text in sent_tokenize(x.text):
+                    o = MultimodalObject(page=x.page, text=self.truncate_text(text))
+                    groups.setdefault(i, []).append(o)
+
+        # If an object text is split into multiple parts, then we select the highest scoring part
+        for i, objects in groups.items():
+            embeds = np.stack([self.model.encode(x.text) for x in objects])
+            # noinspection PyTypeChecker
+            array = cos_sim(query_embeds, embeds)
+            assert array.ndim == 2
+            scores = array.tolist()[0]
+            j = scores.index(max(scores))
+            doc.objects[i].score = max(scores)
+            doc.objects[i].snippet = objects[j].text
+
         return doc
 
 
@@ -50,7 +85,7 @@ def test_retriever(
     name: str = "clip_text",
     query: str = "How many dogs are there?",
     image_url: str = "https://github.com/UKPLab/sentence-transformers/raw/master/examples/applications/image-search/two_dogs_in_snow.jpg",
-    **kwargs
+    **kwargs,
 ):
     generator = GeminiVisionModel()
     retriever = select_retriever(name, **kwargs)
