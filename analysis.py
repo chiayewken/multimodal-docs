770a504c9e9f4f2547db50d7723e9c57673638f2
chiayewken <chiayewken@gmail.com>
2024-02-27 11:01:44 +0800

    Add show_preds for analysis.py

--- analysis.py
+++ analysis.py
@@ -1,7 +1,17 @@
+import io
+from pathlib import Path
+
 from fire import Fire
 from sentence_transformers import SentenceTransformer, util
 
-from data_loading import load_image_from_url
+from data_loading import (
+    load_image_from_url,
+    convert_text_to_image,
+    MultimodalSample,
+    MultimodalData,
+)
+
+from reportlab.platypus import SimpleDocTemplate, Image as DocImage, Paragraph
 
 
 def test_clip(
@@ -24,6 +34,40 @@ def test_clip(
     cos_scores = util.cos_sim(query, data).squeeze().tolist()
     for i, score in enumerate(cos_scores):
         print(dict(text=texts[i], score=cos_scores[i]))
+
+
+def load_doc_image(text: str, size: int) -> DocImage:
+    image = convert_text_to_image(text)
+    content = io.BytesIO()
+    image.save(content, format=image.format)
+    return DocImage(content, width=size, height=size)
+
+
+def show_preds(path: str, path_out: str, image_size=256):
+    Path(path_out).parent.mkdir(exist_ok=True, parents=True)
+    doc = SimpleDocTemplate(str(path_out), pagesize=(5.5 * 72, 25.5 * 72))
+    story = []
+
+    sample: MultimodalSample
+    for i, sample in enumerate(MultimodalData.load(path).samples):
+        sample.print()
+        story.append(Paragraph(f"<b>Prompt</b>:"))
+        for x in sample.prompt.objects:
+            if x.text:
+                story.append(Paragraph(x.text))
+            if x.image_string:
+                story.append(load_doc_image(x.image_string, image_size))
+
+        story.append(Paragraph(f"<b>Gold Answer</b>: {sample.answer}"))
+        story.append(Paragraph(f"<b>Raw Output</b>: {sample.raw_output}"))
+
+    doc.build(story)
+    print(Path(path_out).absolute())
+
+
+"""
+p analysis.py show_preds outputs/demo/acrv/openai_vision/clip_text/top_k_2.jsonl --path_out renders/demo_openai.pdf
+"""
 
 
 if __name__ == "__main__":
