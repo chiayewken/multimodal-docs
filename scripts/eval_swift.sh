b985fa96fb8037957edda4cb78e512df7f985b9d
chiayewken <chiayewken@gmail.com>
2024-11-04 01:41:44 +0800

    Revert requirements.txt for sanity and support /root/env/bin/python in eval_swift.sh (DLC)

--- scripts/eval_swift.sh
+++ scripts/eval_swift.sh
@@ -4,8 +4,17 @@ data_file=$1
 output_dir=$2
 checkpoint_dir=$3
 
+if [ -f "/root/env/bin/python" ]; then
+    python_source="/root/env/bin/python"
+    swift_source="/root/env/bin/python /root/env/lib/python3.10/site-packages/swift/cli/infer.py"
+else
+    python_source="python"
+    swift_source="swift infer"
+fi
+echo "Using python source: $python_source and swift source: $swift_source"
+
 # Assume that same checkpoint only has one prediction at a time, otherwise conflict will occur
-python data_loading.py make_swift_qwen_data $data_file --path_out $output_dir/eval_inputs.jsonl --is_test
-swift infer --ckpt_dir $checkpoint_dir --val_dataset $output_dir/eval_inputs.jsonl
-python data_loading.py read_swift_qwen_preds $checkpoint_dir $data_file $output_dir/eval_outputs.json
-python evaluation.py run_multi_judge $output_dir/eval_outputs.json
+$python_source data_loading.py make_swift_qwen_data $data_file --path_out $output_dir/eval_inputs.jsonl --is_test
+$swift_source --ckpt_dir $checkpoint_dir --val_dataset $output_dir/eval_inputs.jsonl
+$python_source data_loading.py read_swift_qwen_preds $checkpoint_dir $data_file $output_dir/eval_outputs.json
+$python_source evaluation.py run_multi_judge $output_dir/eval_outputs.json
